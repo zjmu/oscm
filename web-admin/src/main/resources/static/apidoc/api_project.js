385cf11e25fbe8597b97395244944c828e2f6950
zhangwenlong <529179804@qq.com>
2019-05-04 23:20:39 +0800

    ImprovementPlan  MonthReport api接口

--- web-admin/src/main/resources/static/apidoc/api_project.js
+++ web-admin/src/main/resources/static/apidoc/api_project.js
@@ -21,7 +21,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-04-24T13:39:08.159Z",
+    "time": "2019-05-04T15:19:07.267Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
